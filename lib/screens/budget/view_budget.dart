1cff7f56fe04bcd2fcd9ffca165050cd78efda9b
dasunx <mwdasun@gmail.com>
2020-09-28 19:52:46 +0530

    Budget view done

--- lib/screens/budget/view_budget.dart
+++ lib/screens/budget/view_budget.dart
@@ -1,8 +1,11 @@
 import 'package:event_planner/classes/Event.dart';
 import 'package:event_planner/components/EmptyList.dart';
+import 'package:event_planner/components/button.dart';
 import 'package:event_planner/constants.dart';
 import 'package:event_planner/screens/budget/add_budget_screen.dart';
+import 'package:event_planner/screens/guest/view_guests.dart';
 import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
 
 class ViewBudget extends StatefulWidget {
   static const String id = 'view_budget';
@@ -13,12 +16,19 @@ class ViewBudget extends StatefulWidget {
 class _ViewBudgetState extends State<ViewBudget> {
   Event event;
   bool conditionx = false;
+  Map<String, double> dataMap = new Map();
 
   @override
   Widget build(BuildContext context) {
     event = ModalRoute.of(context).settings.arguments;
-    if (event.budget.budget != null && event.budget.paidAmount != null) {
-      conditionx = true;
+    if (event.budget != null) {
+      if (event.budget.budget != null && event.budget.paidAmount != null) {
+        conditionx = true;
+        dataMap.putIfAbsent("paidAmount", () => event.budget.paidAmount);
+        dataMap.putIfAbsent(
+            "balance", () => (event.budget.budget - event.budget.paidAmount));
+        print(conditionx);
+      }
     }
 
     return Scaffold(
@@ -38,11 +48,50 @@ class _ViewBudgetState extends State<ViewBudget> {
                 ),
               ),
             ),
+            Visibility(
+              visible: conditionx,
+              child: Expanded(
+                flex: 2,
+                child: Container(
+                  padding: EdgeInsets.all(15),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      detailsRow("Budget amount",
+                          conditionx ? event.budget.budget.toString() : "0"),
+                      detailsRow(
+                          "paid amount: ",
+                          conditionx
+                              ? event.budget.paidAmount.toString()
+                              : "0"),
+                      detailsRow(
+                          "Balance amount: ",
+                          conditionx
+                              ? (event.budget.budget - event.budget.paidAmount)
+                                  .toString()
+                              : "0"),
+                    ],
+                  ),
+                ),
+              ),
+            ),
             conditionx
                 ? Expanded(
                     flex: 8,
                     child: Container(
-                      child: Text("Chart will go here"),
+                      child: Center(
+                          child: PieChart(
+                        dataMap: dataMap,
+                        legendFontColor: Colors.blueGrey[900],
+                        legendFontSize: 14.0,
+                        legendFontWeight: FontWeight.w500,
+                        animationDuration: Duration(milliseconds: 800),
+                        chartLegendSpacing: 32.0,
+                        chartRadius: MediaQuery.of(context).size.width / 2.7,
+                        showChartValuesInPercentage: true,
+                        showChartValues: true,
+                        chartValuesColor: Colors.blueGrey[900].withOpacity(0.9),
+                      )),
                     ),
                   )
                 : EmptyList(
@@ -53,9 +102,41 @@ class _ViewBudgetState extends State<ViewBudget> {
                     },
                     buttonText: "Add budget",
                   ),
+            Visibility(
+              visible: conditionx,
+              child: Expanded(
+                flex: 1,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  child: Tooltip(
+                    message: "Go to dashboard page to view full details",
+                    child: button(
+                      title: "View Dashboard",
+                      onPress: () {
+                        Navigator.pushNamed(context, ViewGuests.id,
+                            arguments: event);
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
+    );
+  }
+
+  Widget detailsRow(String title, String message) {
+    return Row(
+      children: [
+        Text(
+          title,
+          style: kLabelTextStyle.copyWith(color: Colors.black),
+        ),
+        Spacer(),
+        Text(message, style: kLabelTextStyle.copyWith(color: Colors.black)),
+      ],
     );
   }
 }
